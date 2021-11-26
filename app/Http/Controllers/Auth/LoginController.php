dbc70431fb8ce3015587c883c5dbaac9890672f2
Adel Basiony <adel.basiony@gmail.com>
2021-11-26 15:19:15 +0200

    Create Authentications for Register, Login & Logout Forms

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index () {
+        return view('auth.login');
+    }
+
+    public function store (Request $request){
+        $this->validate($request, [
+            'username' => 'required|max:255',
+            'password' => 'required'
+        ]);
+
+        if(!Auth::attempt($request->only('username', 'password'))) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('posts.index');
+// });
